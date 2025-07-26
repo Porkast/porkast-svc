97abbb0160b76f4bd18389fc4dfdb30e764ace53
Chenjunqian <chenjunqian0810@foxmail.com>
2025-07-26 16:32:45 +0000

    fix: start all cron job

--- src/jobs/job_register.ts
+++ src/jobs/job_register.ts
@@ -1,23 +1,17 @@
 import Baker from 'cronbake';
 import { updateUserSubscription } from './user_sub_update';
 
-
-var baker: Baker
-
 export function IniteBakerJobs() {
-    baker = Baker.create()
-    registerJob()
-}
-
-export function registerJob() {
+    const baker = Baker.create()
     // Run user subscription job every hour
     console.log('Add user subscription job');
     baker.add({
         name: 'user-sub-update',
-        cron: "0 0 * * *",
+        cron: "0 0 * * * *",
         callback: async () => {
             console.log('Run user subscription job every hour');
             await updateUserSubscription();
         }
     })
+    baker.bakeAll()
 }
